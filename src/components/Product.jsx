79bded0af9a0be0ab228aaae5a9da20b47de379c
Mim <mimlyrics8@gmail.com>
2024-01-23 23:24:51 +0100

    update product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ const Product = () => {
   return (
     <div>
       <h1>You had access to this route, Great !</h1>
-      <h1 className="text-lg my-1 font-bold ">Modify Product hahaha !!</h1>
+      <h1 className="text-lg my-1 font-bold ">Modify Product hahaha_ useless sentence !!</h1>
     </div>
   )
 }
